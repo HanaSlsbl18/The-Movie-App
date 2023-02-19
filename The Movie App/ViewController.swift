4e365406f19b344910680d9b6d07746c36c77aae
HanaSlsbl18 <hanasalsabila18@gmail.com>
2023-02-19 14:47:56 +0700

    Initial Commit

---
+++ The Movie App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie App
+//
+//  Created by Hana Salsabila on 19/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
